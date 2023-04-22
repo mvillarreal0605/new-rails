e502786d1e7ee2a5206442af71f8e55163ddd18c
Michael Villarreal <mvillarreal0605@gmail.com>
2023-04-21 23:39:11 -0300

    banner adjustments

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -8,9 +8,11 @@ export default class extends Controller {
 
   updateNavbar() {
     if (window.scrollY >= window.innerHeight) {
-      this.element.classList.add("navbar-lewagon-white")
+      this.element.classList.add('navbar-lewagon-white');
+      this.element.classList.remove('navbar-dark');
     } else {
-      this.element.classList.remove("navbar-lewagon-white")
+      this.element.classList.remove('navbar-lewagon-white');
+      this.element.classList.add('navbar-dark');
     }
   }
 }
